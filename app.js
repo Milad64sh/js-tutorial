69a7642beaa658f48d0f3dd08d869231a0c74f3d
Milad64sh <m.shalikarian@gmail.com>
2022-04-01 11:13:23 -0700

    added exercises

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-function morning(name){
-
- return `good morning ${name.toUpperCase()}`;
+const gas = [20, 30, 40, 50];
+const food = [34, 54, 63, 120];
+function calculateTotal(arr){
+ total = 0;
+ for (i = 0; i < arr.length; i ++){
+ total += arr[i]
+ }
+  return total;
 }
-
-
-function greet(name, cb){
- const myName = 'john';
- console.log(`${cb(name)}, my name is ${myName}`);
-
-}
-
-
-greet('bobo',morning);
-greet('peter',morning);
+const totalGas = calculateTotal(gas);
+const totalFood = calculateTotal(food);
+console.log({
+ gas: totalGas,
+ food: totalFood
+});
