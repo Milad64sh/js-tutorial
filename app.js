d6217e1ad721bbc50170e476eb483b1d7b401416
Milad64sh <m.shalikarian@gmail.com>
2022-04-02 07:17:14 -0700

    map method

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const people = [
  {name: 'bob', age: 20, position: 'Developer'},
  {name: 'John', age: 25, position: 'Designer'},
  {name: 'Susy', age: 30, position: 'the boss'},
+ {name: 'Anna', age: 35, position: 'the boss'},
 ];
 
-function showPerson(person){
- console.log(person);
-}
- 
-people.forEach(showPerson);
+const ages = people.map(function (person){
+ return person.age + 20;
+});
+const newPeople = people.map(function (person){
+ return {firstName:person.name.toUpperCase()}
+})
+console.log(ages);
