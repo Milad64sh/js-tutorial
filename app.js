512f74ac6f4610177e8c0bd528277e3be0199aaa
Milad64sh <m.shalikarian@gmail.com>
2022-04-01 17:04:00 -0700

    forEach method

--- app.js
+++ app.js
@@ -1,15 +1,11 @@
-const gas = [20, 30, 40, 50];
-const food = [34, 54, 63, 120];
-function calculateTotal(arr){
- total = 0;
- for (i = 0; i < arr.length; i ++){
- total += arr[i]
- }
-  return total;
+const people = [
+ {name: 'bob', age: 20, position: 'Developer'},
+ {name: 'John', age: 25, position: 'Designer'},
+ {name: 'Susy', age: 30, position: 'the boss'},
+];
+
+function showPerson(person){
+ console.log(person);
 }
-const totalGas = calculateTotal(gas);
-const totalFood = calculateTotal(food);
-console.log({
- gas: totalGas,
- food: totalFood
-});
+ 
+people.forEach(showPerson);
