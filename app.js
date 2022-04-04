a4abfd94a623e89324574d3a962ca547f735e2c5
Milad64sh <m.shalikarian@gmail.com>
2022-04-04 16:01:23 -0700

    DOM tutorial

--- app.js
+++ app.js
@@ -1,17 +1,11 @@
-const people = [
- {name: 'john', age: 25, position: 'developer', id:1},
- {name: 'jimmy', age: 30, position: 'designer', id: 2},
- {name: 'rose', age: 35, position: 'boss', id: 3},
- {name: 'michael', age: 40, position: 'ceo', id: 4},
-];
+const h1 = document.getElementById('title');
+h1.style.color = 'red';
 
 
-const youngPeople = people.filter(function(person){
- return person.age <= 30;
-});
+const btn = document.getElementById('btn');
+btn.style.backgroundColor = 'blue';
+btn.style.color = 'white';
 
-const positions = people.filter(function(person){
- return person.position === 'developer';
-});
-console.log(positions);
-console.log(youngPeople);
+
+document.getElementById('btn').style.backgroundColor = 'blue';
+document.getElementById('btn').style.color = 'white';
