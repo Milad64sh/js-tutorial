c07821b3b04e185c9381819a22c8e40082ae68ae
Milad64sh <m.shalikarian@gmail.com>
2022-04-05 13:58:13 -0700

    navigate the DOM

--- app.js
+++ app.js
@@ -1,14 +1,7 @@
 const result = document.querySelector('#result');
-result.style.backgroundColor = 'blue';
 
-const item = document.querySelector('.special');
+const children = result.children;
+console.log(children);
 
-const lastItem = document.querySelector('li:last-child');
-console.log(lastItem);
-
-const list = document.querySelectorAll('.special');
-
-list.forEach(function(item){
- console.log(item);
- item.style.color = 'yellow';
-});
+console.log(result.firstChild);
+console.log(result.lastChild);
