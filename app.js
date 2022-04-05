1209301ebb1336fa0e1e6b9ebd08ee81c3a5aef1
Milad64sh <m.shalikarian@gmail.com>
2022-04-05 07:32:56 -0700

    tutorial continues

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
 const headings = document.getElementsByTagName('h2');
 console.log(headings);
 headings[0].style.color = 'red';
-console.log(headings.length);
+// console.log(headings.length);
+const items = document.getElementsByTagName('li');
+
+items[2].style.color = 'orange';
+const betterItems = [...items];
+betterItems.forEach(function(item){
+ console.log(item);
+})
+
+// items.forEach(function(item){
+//  console.log(item);
+// })
