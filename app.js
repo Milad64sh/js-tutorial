da6aecf9aa642f5acd28048fa1cbdd9a6870f471
Milad64sh <m.shalikarian@gmail.com>
2022-04-08 10:04:24 -0700

    get and set attributes

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
-const heading = document.querySelector('h2');
-console.log(heading.parentElement.parentElement);
+
+const first = document.querySelector('.first');
+const idValue = first.getAttribute('id');
+// console.log(idValue);
+
+const link = document.getElementById('link');
+const showLink = link.getAttribute('href');
+// console.log(showLink);
+
+
+const last = link.nextElementSibling;
+last.setAttribute('class', 'first');
+last.textContent = 'i also have a class of first';
+console.log(last);
+// 
