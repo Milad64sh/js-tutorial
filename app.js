b7a802fe8b67969ef6e9b0a45543a5eb4eb9a397
Milad64sh <m.shalikarian@gmail.com>
2022-04-08 08:00:12 -0700

    dom navigation

--- app.js
+++ app.js
@@ -1,8 +1,2 @@
-const people = [
- {name: 'bob', age: 20, position: 'Developer'},
- {name: 'John', age: 25, position: 'Designer'},
- {name: 'Susy', age: 30, position: 'the boss'},
-];
-people.forEach(function(item){
-console.log(item.position.toUpperCase());
-});
+const heading = document.querySelector('h2');
+console.log(heading.parentElement.parentElement);
