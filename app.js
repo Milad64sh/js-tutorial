f57f42c8374db3e9a4648ac5c56a423e74c7df47
Milad64sh <m.shalikarian@gmail.com>
2022-04-09 15:32:17 -0700

    createElement createTextNode

--- app.js
+++ app.js
@@ -1,23 +1,17 @@
-const first = document.getElementById('first');
-const second = document.getElementById('second');
-const third = document.getElementById('third');
+const result = document.querySelector('#result');
+
+const bodyDive = document.createElement('div');
+
+const text = document.createTextNode('a simple body div');
+bodyDive.appendChild(text);
+document.body.appendChild(bodyDive);
+
+const heading = document.createElement('h2');
+const headingText = document.createTextNode('dynamic heading');
+heading.appendChild(headingText);
+heading.classList.add('blue');
+
+result.appendChild(heading);
 
 
-third.classList.add('colors','text');
-third.classList.remove('colors');
-const classValue = third.classList;
-console.log(classValue);
-
-
-second.className = 'colors text';
-second.className = 'text';
-
-
-// const classValue = first.className;
-
-let result = third.classList.contains('text'); 
-if(result){
- console.log('hello world');
-} else{
- console.log('does not have the class');
-}
+console.log(result.children);
