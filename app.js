3c109000cef61a4addd0eacebf6937ff106431e6
Milad64sh <m.shalikarian@gmail.com>
2022-04-09 15:52:51 -0700

    insertBefore

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
 const result = document.querySelector('#result');
-
+const first = document.querySelector('.red');
 const bodyDive = document.createElement('div');
 
 const text = document.createTextNode('a simple body div');
 bodyDive.appendChild(text);
-document.body.appendChild(bodyDive);
+document.body.insertBefore(bodyDive,result);
 
 const heading = document.createElement('h2');
 const headingText = document.createTextNode('dynamic heading');
 heading.appendChild(headingText);
 heading.classList.add('blue');
 
-result.appendChild(heading);
-
+result.insertBefore(heading,first);
 
 console.log(result.children);
