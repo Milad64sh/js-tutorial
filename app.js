338738381a299cee548f7f554c5c1b523bfb6551
Milad64sh <m.shalikarian@gmail.com>
2022-04-10 11:35:06 -0700

    add logic to event

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 
 const btn = document.querySelector('.btn');
-
 const heading = document.querySelector('h2');
-btn.addEventListener('click', function(){
-heading.classList.add('red');
-})
+
+
+function changeColors(){
+ let hasClass = heading.classList.contains('red');
+ if(hasClass){
+  heading.classList.remove('red');
+ }else{
+  heading.classList.add('red');
+ }
+}
+
+
+btn.addEventListener('click', changeColors);
