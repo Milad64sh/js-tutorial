9a0e02d373e4e9702d27e3505be57202600d3283
Milad64sh <m.shalikarian@gmail.com>
2022-04-10 11:07:54 -0700

    events - click events

--- app.js
+++ app.js
@@ -1,14 +1,7 @@
-const list = document.getElementById('first');
-const div = document.getElementById('second');
-const item = document.querySelector('.item');
 
-console.log(div.textContent);
-console.log(list.innerHTML);
+const btn = document.querySelector('.btn');
 
-
-const randomVar = 'random value';
-
-const ul = document.createElement('ul');
-ul.innerHTML = ` <li class="item">${randomVar}</li>
- <li>list item</li> <li>list item</li>`;
-document.body.appendChild(ul);
+const heading = document.querySelector('h2');
+btn.addEventListener('click', function(){
+heading.classList.add('red');
+})
