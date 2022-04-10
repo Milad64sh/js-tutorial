71903325fbd407de82e8eccf73fa80d93aa326fa
Milad64sh <m.shalikarian@gmail.com>
2022-04-09 21:15:53 -0700

    remove

--- app.js
+++ app.js
@@ -1,16 +1,5 @@
+
+
 const result = document.querySelector('#result');
-const first = document.querySelector('.red');
-const bodyDive = document.createElement('div');
-
-const text = document.createTextNode('a simple body div');
-bodyDive.appendChild(text);
-document.body.insertBefore(bodyDive,result);
-
-const heading = document.createElement('h2');
-const headingText = document.createTextNode('dynamic heading');
-heading.appendChild(headingText);
-heading.classList.add('blue');
-
-result.insertBefore(heading,first);
-
-console.log(result.children);
+const heading = result.querySelector('h1');
+result.removeChild(heading); 
