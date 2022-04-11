2ecd45b201d1e5142f79e42285357952e76cd9d5
Milad64sh <m.shalikarian@gmail.com>
2022-04-11 14:13:04 -0700

    repeat DOM

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
+const first = document.querySelector('#first');
+const second = document.querySelector('#second');
+const third = document.querySelector('#third');
 
-const btn = document.querySelector('.btn');
-const heading = document.querySelector('h2');
 
+first.className = 'colors';
+first.className = 'text colors';
 
-function changeColors(){
- let hasClass = heading.classList.contains('red');
- if(hasClass){
-  heading.classList.remove('red');
- }else{
-  heading.classList.add('red');
- }
+second.classList.add('text','colors');
+
+second.classList.remove('colors');
+let result = second.classList.contains('text');
+if(result){
+ console.log('hello world');
+} else{
+ console.log('not valued');
 }
-
-
-btn.addEventListener('click', changeColors);
