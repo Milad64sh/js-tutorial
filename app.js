76f2a4fab4311e46c1a082a53886e8ed19d04e20
Milad64sh <m.shalikarian@gmail.com>
2022-04-12 16:25:33 -0700

    propagation example

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 const container = document.querySelector('.container');
-const list = document.querySelector('.list-items');
+const btn = document.querySelector('.btn');
+// const heading = document.querySelector('.heading');
 
-function showBubbling(e){
- console.log('current target',e.currentTarget);
- console.log('target',e.target);
- if(e.target.classList.contains('link')){
-  console.log('you clicked on the link');
+
+function sayHello(){
+ console.log('hello there');
+}
+
+btn.addEventListener('click',function(){
+ const element = document.createElement('h1');
+ element.classList.add('heading');
+ element.textContent = `inside the container`;
+ container.appendChild(element);
+});
+container.addEventListener('click', function(e){
+ if(event.target.classList.contains('heading')){
+  console.log('hello there');
  }
-}
-function stopPropagation(e){
- e.stopPropagation();
-}
+});
 
-list.addEventListener('click', stopPropagation);
+// heading.addEventListener('click', sayHello);
