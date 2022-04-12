2b3c76979847c0675becda035a80bdebef2426a2
Milad64sh <m.shalikarian@gmail.com>
2022-04-11 20:55:53 -0700

    event object

--- app.js
+++ app.js
@@ -1,22 +1,19 @@
-const heading = document.querySelector('h2');
+const heading = document.querySelector('h1');
 const btn = document.querySelector('.btn');
+const link = document.querySelector('#link');
 
+function someFunc(e){
+ e.preventDefault();
+}
+link.addEventListener('click',someFunc);
 
-
-heading.addEventListener('mouseenter',function(){
- heading.classList.add('blue');
-})
-heading.addEventListener('mouseleave',function(){
- heading.classList.remove('blue');
-})
-
-
-btn.addEventListener('click', function(){
- console.log('you clicked me');
+heading.addEventListener('click', function(evt){
+console.log(evt.currentTarget);
+console.log(this);
 });
-btn.addEventListener('mousedown', function(){
- console.log('down');
+
+btn.addEventListener('click', function(evt){
+evt.currentTarget.classList.add('blue');
+console.log(evt.type);
 });
-btn.addEventListener('mouseup', function(){
- console.log('up');
-});
+
