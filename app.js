081f4b73b51a5ab7f9dbb7bbe42c09dde34f20c0
Milad64sh <m.shalikarian@gmail.com>
2022-04-12 10:10:40 -0700

    target

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const btns = document.querySelectorAll('.btn');
 
 btns.forEach(function(btn){
 btn.addEventListener('click',function(e){
-console.log(e.currentTarget);
-e.currentTarget.style.color = 'green';
+ console.log(e.target);
+ e.target.style.color = 'green'; 
 });
 });
