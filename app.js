8b1b050d9b4381f1686c46b415fa50df53a7d113
Milad64sh <m.shalikarian@gmail.com>
2022-04-14 15:58:42 -0700

    counter

--- app.js
+++ app.js
@@ -1,18 +1,29 @@
-const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+let count = 0;
 
-const btn = document.getElementById('btn');
-const color = document.querySelector('.color');
+const value = document.querySelector('#value');
+const btns = document.querySelectorAll('.btn');
 
-btn.addEventListener('click',function(){
- let hexColor = '#';
- for(let i = 0; i<6; i++){
-  hexColor += hex[getRandomNumber()];
- }
- color.textContent = hexColor;
- document.body.style.backgroundColor = hexColor; 
+btns.forEach(function (btn){
+ btn.addEventListener('click', function(e){
+  const styles = e.currentTarget.classList;
+  if(styles.contains('decrease')){
+   count --;
+  }
+  else if (styles.contains('increase')){
+   count ++;
+  }
+  else {
+   count = 0;
+  }
+  if(count>0){
+   value.style.color = 'green';
+  }
+  if(count < 0){
+   value.style.color = 'red';
+  }
+  if(count === 0){
+   value.style.color = 'black';
+  }
+  value.textContent = count;
+ })
 });
-
-function getRandomNumber(){
- return Math.floor(Math.random() *hex.length)
-};
-
