073024427fb1e161b28d46680cd031f46526d723
Milad64sh <m.shalikarian@gmail.com>
2022-04-15 09:14:33 -0700

    color flipper repeat

--- app.js
+++ app.js
@@ -1,29 +1,16 @@
-let count = 0;
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 
-const value = document.querySelector('#value');
-const btns = document.querySelectorAll('.btn');
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
 
-btns.forEach(function (btn){
- btn.addEventListener('click', function(e){
-  const styles = e.currentTarget.classList;
-  if(styles.contains('decrease')){
-   count --;
-  }
-  else if (styles.contains('increase')){
-   count ++;
-  }
-  else {
-   count = 0;
-  }
-  if(count>0){
-   value.style.color = 'green';
-  }
-  if(count < 0){
-   value.style.color = 'red';
-  }
-  if(count === 0){
-   value.style.color = 'black';
-  }
-  value.textContent = count;
- })
+btn.addEventListener('click',function(){
+ let hexColor = '#';
+ for(let i = 0; i<6; i++){
+  hexColor += hex[getRandomNumber()];
+ }
+ color.textContent = hexColor;
+ document.body.style.backgroundColor = hexColor; 
 });
+function getRandomNumber(){
+ return Math.floor(Math.random() * hex.length);
+};
