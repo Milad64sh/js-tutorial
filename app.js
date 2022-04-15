5d4d3cb371f4016e0fb0fbb323425579a8b948c1
Milad64sh <m.shalikarian@gmail.com>
2022-04-15 09:41:43 -0700

    counter repeat

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
-const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+let count = 0;
 
-const btn = document.getElementById('btn');
-const color = document.querySelector('.color');
+const value = document.querySelector('#value');
+const btns = document.querySelectorAll('.btn');
 
-btn.addEventListener('click',function(){
- let hexColor = '#';
- for(let i = 0; i<6; i++){
-  hexColor += hex[getRandomNumber()];
+btns.forEach(function(btn){
+ btn.addEventListener('click',function(e){
+const styles = e.currentTarget.classList;
+if(styles.contains('decrease')){
+ count --;
+}else if(styles.contains('increase')){
+ count ++;
+}else{
+ count = 0;
+}
+ if(count<0){
+  value.style.color = 'red';
+ }if(count>0){
+  value.style.color = 'green';
+ }if (count === 0){
+  value.style.color = 'black';
  }
- color.textContent = hexColor;
- document.body.style.backgroundColor = hexColor; 
-});
-function getRandomNumber(){
- return Math.floor(Math.random() * hex.length);
-};
+  value.textContent = count;
+ })
+})
