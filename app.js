1f0b120c1accee427c6a0be17a0ca7f23daf7ac3
Milad64sh <m.shalikarian@gmail.com>
2022-04-17 15:50:52 -0700

    modal project

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-const toggleBtn = document.querySelector('.sidebar-toggle');
+const modalBtn = document.querySelector('.modal-btn');
+const modal = document.querySelector('.modal-overlay');
 const closeBtn = document.querySelector('.close-btn');
-const sidebar = document.querySelector('.sidebar');
 
-toggleBtn.addEventListener('click',function(){
-sidebar.classList.toggle('show-sidebar');
+modalBtn.addEventListener('click',function(){
+modal.classList.add('open-modal');
 });
 closeBtn.addEventListener('click',function(){
- sidebar.classList.remove('show-sidebar');
-});
+modal.classList.remove('open-modal');
+ });
