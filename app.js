671a2cefa9b249b3ef05d7eda22dcf56ad7425fd
Milad64sh <m.shalikarian@gmail.com>
2022-04-17 15:00:19 -0700

    sidebar

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
-const navToggle = document.querySelector('.nav-toggle');
-const links = document.querySelector('.links');
+const toggleBtn = document.querySelector('.sidebar-toggle');
+const closeBtn = document.querySelector('.close-btn');
+const sidebar = document.querySelector('.sidebar');
 
-navToggle.addEventListener('click',function(){
- links.classList.toggle('show-links');
+toggleBtn.addEventListener('click',function(){
+ if(sidebar.classList.contains('show-sidebar')){
+  sidebar.classList.remove('show-sidebar');
+ }else{
+  sidebar.classList.add('show-sidebar');
+ }
 });
-
