454199e2cb1d48ea58a560c57c6dd4874f91baa4
Milad64sh <m.shalikarian@gmail.com>
2022-04-20 07:37:51 -0700

    get unique categories

--- app.js
+++ app.js
@@ -76,9 +76,21 @@ const menu = [
 const sectionCenter = document.querySelector('.section-center');
 const filterBtns = document.querySelectorAll('.filter-btn');
 
+// load items
 window.addEventListener('DOMContentLoaded',function(){
 displayMenuItems(menu);
+const categories = menu.reduce(function(values,item){
+ if(!values.includes(item.category)){
+   values.push(item.category);
+ }
+ return values
+},['all']);
+console.log(categories);
 });
+
+
+
+
 
 // filter items
 filterBtns.forEach(function(btn){
