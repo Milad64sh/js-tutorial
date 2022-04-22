4ad9ec80f33267f4d337f0448d043ed338a8c4a9
Milad64sh <m.shalikarian@gmail.com>
2022-04-22 09:47:38 -0700

    scroll p setting htmml

--- app.js
+++ app.js
@@ -1,20 +1,3 @@
-const btn = document.querySelector('.switch-btn');
-const container = document.querySelector('.video-container');
-
-btn.addEventListener('click', function(){
-if(!btn.classList.contains('slide')){
- btn.classList.add('slide');
- Video.pause();
-}else{
- btn.classList.remove('slide');
- video.play();
-}
-});
-
-// preloader
-const preloader = document.querySelector('.preloader');
-
-window.addEventListener('load',function(){
- preloader.classList.add('lide-preloader');
-})
-
+// ********** set date ************
+const date = document.getElementById('date');
+date.innerHTML = new Date().getFullYear();
