c4ead15782915dc03f9ac630f35cd6762c0921ff
Milad64sh <m.shalikarian@gmail.com>
2022-04-26 12:19:06 -0700

    setting alert

--- app.js
+++ app.js
@@ -13,14 +13,26 @@ let editFlag = false;
 let editID = '';
 // ****** EVENT LISTENERS **********
 // submit form
-form.addEventListener('submit'.addItem)
+form.addEventListener('submit', addItem)
 
 // ****** FUNCTIONS **********
-function addItem(e) {
+function addItem(e){
  e.preventDefault();
  const value = grocery.value;
  const id = new Date().getTime().toString;
- if (value !== '' && editFlag === false) { }
- else if (value !== '' && editFlag === true) { }
- else { }
+ if(value && !editFlag){}
+ else if(value && editFlag){}
+ else{
+ displayAlert('please enter value', 'danger');
+ }
 };
+// display alert
+function displayAlert(text,action){
+  alert.textContent = text;
+  alert.classList.add(`alert-${action}`);
+  // rempve alert
+  setTimeout(function(){
+    alert.textContent = '';
+    alert.classList.remove(`alert-${action}`);
+  },1000);
+}
