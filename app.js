0bf3bd19e7f5d29550c81dac191ebc66e29da945
Milad64sh <m.shalikarian@gmail.com>
2022-04-26 16:23:48 -0700

    add item

--- app.js
+++ app.js
@@ -20,7 +20,34 @@ function addItem(e){
  e.preventDefault();
  const value = grocery.value;
  const id = new Date().getTime().toString;
- if(value && !editFlag){}
+ if(value && !editFlag){
+   const element = document.createElement('article');
+  //  add class
+  element.classList.add('grocery-item');
+  // add id
+  const attr = document.createAttribute('data-id');
+  attr.value = id;
+  element.setAttributeNode(attr);
+  element.innerHTML = `<p class="title">${value}</p>
+            <div class="btn-container">
+              <button type="button" class="edit-btn">
+                <i class="fas fa-edit"></i>
+              </button>
+              <button type="button" class="delete-btn">
+                <i class="fas fa-trash"></i>
+              </button>
+            </div>`;
+  // append child
+  list.appendChild(element);
+  // display alert
+  displayAlert('item added to the list', 'success');
+  // show container
+  container.classList.add('show-container');
+  // add to local storage
+  addToLocalStorage(id,value);
+  // set back to default
+  setBackToDefault();
+ }
  else if(value && editFlag){}
  else{
  displayAlert('please enter value', 'danger');
@@ -36,3 +63,12 @@ function displayAlert(text,action){
     alert.classList.remove(`alert-${action}`);
   },1000);
 }
+// set back to defaul
+function setBackToDefault(){
+  console.log('set back to default');
+}
+// ****** LOCAL STORAGE **********
+function addToLocalStorage(id,value){
+  console.log('added to local storage');
+}
+// ****** SETUP ITEMS **********
