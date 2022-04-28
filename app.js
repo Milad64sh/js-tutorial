d21cfbeef703ac2f0eb805572bc6ca78186a51a9
Milad64sh <m.shalikarian@gmail.com>
2022-04-28 13:01:45 -0700

    grocery bud complete

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-/ ****** SELECT ITEMS **********
+// / ****** SELECT ITEMS **********
 const alert = document.querySelector('.alert');
 const form = document.querySelector('.grocery-form');
 const grocery = document.getElementById('grocery');
