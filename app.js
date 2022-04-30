27e7d1c3c2c7b2ce3d6d4e50caabf43c1359a037
Milad64sh <m.shalikarian@gmail.com>
2022-04-29 17:35:38 -0700

    this keyword

--- app.js
+++ app.js
@@ -1,25 +1,16 @@
-
-const age = 60
-const person = {
- 'random-value': 'random',
- name : 'john',
- age: age,
- married: true,
- siblings: ['anna','peter'],
- greet: function (name){
-  console.log(`hello, ma name is ${name}`);
- },
- sayHello(name){
-  console.log(`hello, my name is ${name}`);
- },
- job:{
-  title: 'developer',
-  compnay: {
-   name:'coding addict',
-   address:'123,landline road, LA',
-  },
+const bob = {
+ firstName : 'bob',
+ lastName : 'sanders',
+ fullName : function(){
+  console.log(`My full name is ${this.firstName} ${this.lastName}`);
  },
 };
-let random = 'random-value';
-random = 'age';
-console.log(person[random]);
+const john = {
+ firstName : 'john',
+ lastName : 'Anderson',
+ fullName : function(){
+  console.log(`My full name is ${this.firstName} ${this.lastName}`);
+ },
+};
+john.fullName();
+bob.fullName();
