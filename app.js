45e9ddf7e7940122258282d690ad454b3ac40b20
Milad64sh <m.shalikarian@gmail.com>
2022-05-06 08:59:29 -0700

    close modal

--- app.js
+++ app.js
@@ -18,7 +18,10 @@ this.nextBtn = getElement('.next-btn');
 this.prevBtn = getElement('.prev-btn');
 // let self = this; 
 // bind functions
-// this.openModal = this.openModal.bind(this); 
+// this.openModal = this.openModal.bind(this);
+this.closeModal = this.closeModal.bind(this);
+this.nextImage = this.nextImage.bind(this);
+this.prevImage = this.prevImage.bind(this);
 // container event
 this.container.addEventListener('click', function(e){
 // self.openModal();
@@ -34,13 +37,26 @@ Gallery.prototype.openModal = function(selectedImage, list){
  this.modalImages.innerHTML = list.map(function(image){
  return `<img src="${image.src}" title="${image.title}" data-id="${image.dataset.id}" class="${selectedImage.dataset.id === image.dataset.id ? 'modal-img selected' : 'modal-img'}"/>`
  }).join('');
- this.modal.classList.add('open'); 
+ this.modal.classList.add('open');
+ this.closeBtn.addEventListener('click',this.closeModal); 
+ this.nextBtn.addEventListener('click',this.nextImage); 
+ this.prevBtn.addEventListener('click',this.prevModal);
 }
 
 Gallery.prototype.setMainImage = function(selectedImage) {
 this.modalImg.src = selectedImage.src;
 this.imageName.textContent = selectedImage.title;
 };
+
+Gallery.prototype.closeModal = function(){
+  this.modal.classList.remove('open');
+   this.closeBtn.removeEventListener('click',this.closeModal); 
+ this.nextBtn.removeEventListener('click',this.nextImage); 
+ this.prevBtn.removeEventListener('click',this.prevModal);
+}
+Gallery.prototype.nextImage = function(){}
+Gallery.prototype.prevImage = function(){}
+
 
 const nature = new Gallery(getElement('.nature'));
 const city = new Gallery(getElement('.city'));
