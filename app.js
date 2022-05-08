afa35eaaa2c6842d0ec23197da215a0721dc1110
Milad64sh <m.shalikarian@gmail.com>
2022-05-08 07:55:21 -0700

    setting up instances

--- app.js
+++ app.js
@@ -1,13 +1,3 @@
-function Counter(classes, value){
- console.log(classes,value);
-}
-
-
-
-firstCounter = new Counter (getClass('.first-counter', 0));
-secondCounter = new Counter (getClass('.second-counter', 50));
-
-
 function getClass (selection){
  const classes =document.querySelector(selection);
  if(classes){
@@ -17,3 +7,20 @@ function getClass (selection){
   `Please check "${classes}" selector, no such classes exist`
  );
 }
+
+
+function Counter (classes, value){
+this.counter = classes;
+this.value = value;
+this.resetBtn = classes.querySelector('.reset');
+this.increaseBtn = classes.querySelector('.increase');
+this.decreaseBtn = classes.querySelector('.decrease');
+this.valueDOM = classes.querySelector('.value');
+this.valueDOM.textContent = this.value;
+}
+
+
+
+firstCounter = new Counter (getClass('.first-counter', 0));
+secondCounter = new Counter (getClass('.second-counter', 50));
+
