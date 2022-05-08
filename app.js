340637e8f7aa69203834138f4d1093bd415be84f
Milad64sh <m.shalikarian@gmail.com>
2022-05-08 11:46:22 -0700

    gallery pr rp

--- app.js
+++ app.js
@@ -1,47 +1,7 @@
-function getClass (selection){
- const classes =document.querySelector(selection);
- if(classes){
-  return classes
- }
- throw new Error (
-  `Please check "${classes}" selector, no such classes exist`
- );
+function getElement (selection){
+  const element = document.querySelector(selection);
+  if(element){
+    return element
+  }
+  throw new Error(`please check "${selection}", no such element exists`);
 }
-
-
-function Counter (classes, value){
-this.counter = classes;
-this.value = value;
-this.resetBtn = classes.querySelector('.reset');
-this.increaseBtn = classes.querySelector('.increase');
-this.decreaseBtn = classes.querySelector('.decrease');
-this.valueDOM = classes.querySelector('.value');
-this.valueDOM.textContent = this.value;
-
-this.increase  = this.increase.bind(this);
-this.decrease  = this.decrease.bind(this);
-this.reset  = this.reset.bind(this);
-
-this.increaseBtn.addEventListener('click', this.increase);
-this.decreaseBtn.addEventListener('click', this.decrease);
-this.resetBtn.addEventListener('click', this.reset);
-}
-Counter.prototype.increase = function (){
- console.log(this);
- this.value++;
- this.valueDOM.textContent = this.value;
-}
-Counter.prototype.decrease = function (){
- this.value--;
- this.valueDOM.textContent = this.value;
-}
-Counter.prototype.reset = function (){
- this.value = 0;
- this.valueDOM.textContent = this.value;
-}
-
-
-const firstCounter = new Counter (getClass('.first-counter'), 0);
-const secondCounter = new Counter (getClass('.second-counter'), 50);
-
-secondCounter.decrease();
