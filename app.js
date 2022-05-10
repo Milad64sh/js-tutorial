eb0d08f9146022fce46dfdf40d7456d680b1c022
Milad64sh <m.shalikarian@gmail.com>
2022-05-10 08:21:43 -0700

    this in arrow func

--- app.js
+++ app.js
@@ -1,42 +1,22 @@
-// function sayHi(){
-//   console.log('hello');
-// }
-// sayHi();
+const bob = {
+  firstName : 'bob',
+  lastName : 'jordan',
+  sayName: function(){
+    console.log(this);
+    setTimeout(() =>{
+      console.log(`Hello, my name is ${this.firstName} ${this.lastName}`);
+    },2000);
+  },
+};
+bob.sayName();
 
-// const hello = function(name){
-//   console.log(`hello ${name}`);
-// }
-// hello('bob');
 
-// function triple(value1,value2){
-//   return value1 * value2 * 3;
-// }
-
-const sayHi = () => console.log('hello');
-sayHi();
-
-const double = value => value * 2;
-const num = double(4);
-console.log(num);
-
-const multiply = (num1,num2) => {
-  const result = num1* num2;
-  // more code here
-  return result
-}
-
-const sum = multiply(4,6);
-console.log(sum);
-
-// return object
-const object = () => ({name:'john',age:25});
-const person = object();
-console.log(person);
-
-// arrow functions as callback functions
-const numbers = [1,2,3,4,5,6]; 
-const big = numbers.filter((number,index)  => {
-  console.log(index);
- return number>2;
-});
-console.log(big);
+const anna = {
+  firstName : 'anna',
+  lastName : 'sanders',
+  sayName: ()=>{
+    console.log(this);
+    console.log(`Hello, my name is ${this.firstName} ${this.lastName}`);
+  },
+};
+// anna.sayName();
