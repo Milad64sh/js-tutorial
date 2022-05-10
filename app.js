7b1687f82bd3028c705e4b3e7cdc9eeb62de7de7
Milad64sh <m.shalikarian@gmail.com>
2022-05-09 21:16:59 -0700

    arrow function

--- app.js
+++ app.js
@@ -1,14 +1,42 @@
-const person ={
-  name : 'kyle',
-  job : 'developer',
-  hobbies : ['surfing','baking','bowling'],
+// function sayHi(){
+//   console.log('hello');
+// }
+// sayHi();
+
+// const hello = function(name){
+//   console.log(`hello ${name}`);
+// }
+// hello('bob');
+
+// function triple(value1,value2){
+//   return value1 * value2 * 3;
+// }
+
+const sayHi = () => console.log('hello');
+sayHi();
+
+const double = value => value * 2;
+const num = double(4);
+console.log(num);
+
+const multiply = (num1,num2) => {
+  const result = num1* num2;
+  // more code here
+  return result
 }
 
-const result = document.getElementById('result');
+const sum = multiply(4,6);
+console.log(sum);
 
-result.innerHTML = `
-<h2>${person.name}</h2>
-<p>${person.job}</p>
-<ul>${person.hobbies.map(item =>{
-  return `<li>${item}</li>`
-}).join('')}</ul>`
+// return object
+const object = () => ({name:'john',age:25});
+const person = object();
+console.log(person);
+
+// arrow functions as callback functions
+const numbers = [1,2,3,4,5,6]; 
+const big = numbers.filter((number,index)  => {
+  console.log(index);
+ return number>2;
+});
+console.log(big);
