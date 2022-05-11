fae12c3606e918dc3bed0d0523391be3296456f3
Milad64sh <m.shalikarian@gmail.com>
2022-05-11 10:41:14 -0700

    rest operator

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
-const headings = document.querySelectorAll('h1');
-const result = document.getElementById('result');
+// arrays
+const fruit = ['apple','orange','lemon'];
+const [first,...fruits] = fruit;
+console.log(first,fruits);
 
-const text = [...headings].map(item => `<span>${item.textContent}</span>`).join('');
+// objects
+const person = {name: 'john', lastName: 'smith', job: 'developer'};
+const {job,...rest } = person;
+console.log(job,rest);
 
-result.innerHTML = text;
-
+const testScores = [70, 80, 66, 34, 95, 100, 65];
+const getAverage = (name,...scores) =>{
+let total = 0;
+for(const score of scores){
+ total += score;
+}
+console.log(`${name}'s average score is ${total/scores.length}`);
+};
+getAverage(person.name, ...testScores);
