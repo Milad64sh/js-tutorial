574b0d910cd43bfa9f665029a42c714d0c4d3610
Milad64sh <m.shalikarian@gmail.com>
2022-05-12 08:05:42 -0700

    find,findindex,every,some

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
-const p = document.querySelectorAll('p');
-const result = document.getElementById('result');
-const second = document.getElementById('second');
+const people = [
+ {id: 1, name: 'john'},
+ {id: 2, name: 'peter'},
+ {id: 3, name: 'anna'},
+];
 
-let newText = Array.from(p);
-newText = newText.map(item => `<span>${item.textContent}</span>`).join(' ');
-result.innerHTML = newText;
 
-const text = Array.from(document.querySelectorAll('p'), item=>{
-return `<span>${item.textContent}</span>`
-}).join(' ');
-second.innerHTML = text;
+// const anna = people.filter(person => person.name === 'anna');
+// console.log(anna);
+// console.log(anna[0].name);
+// console.log(anna[0].id);
+
+// find
+// const anna = people.find(person => person.name === 'anna');
+// console.log(anna);
+
+const person = people.findIndex(item => item.id === 3);
+console.log(person);
+const newPeople = people.slice(0,person);
+console.log(newPeople);
+
+
+const grades = ['A', 'B', 'A', 'B', 'C'];
+const allGoodGrades = grades.every(grade => grade !== 'C');
+console.log(allGoodGrades);
+
+const goodGrades = ['A', 'B', 'A', 'B'];
+const oneBadGrade = grades.some(grade => grade === 'C');
+console.log(oneBadGrade);
