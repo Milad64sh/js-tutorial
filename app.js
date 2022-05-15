ba0eda84878d3d99f6a5cb4aa670172671a144ed
Milad64sh <m.shalikarian@gmail.com>
2022-05-15 09:25:53 -0700

    recipe async

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
+// make soup
+// boil water 10 min
+// chop carrots
+// add carrots boil for 5 min
+// chop onion
+// add onion boil for 5 min
 
 
-boilWater(0);
+boilWater();
 console.log('chop carrot');
 
-function boilWater(time){
-console.log('boiling...');
- setTimeout(() => {
-    console.log('done');
- }, time);
+function boilWater(){
+   console.log('boiling ...');
+   setTimeout(()=>{
+      console.log('done.');
+      console.log('add carrot.');
+      setTimeout(()=>{
+         console.log('carrot done.');         
+         console.log('add onion.');         
+         
+         setTimeout(() => {
+            console.log('onion done');
+         },5000);
+      },5000);
+      console.log('chop onion');
+   },10000);
 }
