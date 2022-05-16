ff8cb8ba8d9eff2bcefc3a9710c2c4743725bcdd
Milad64sh <m.shalikarian@gmail.com>
2022-05-16 11:08:08 -0700

    add color example

--- app.js
+++ app.js
@@ -1,23 +1,24 @@
 const heading1 = document.querySelector('.one');
 const heading2 = document.querySelector('.two');
 const heading3 = document.querySelector('.three');
-const container = document.querySelector('.img-container');
 const btn = document.querySelector('.btn');
-const url = "https://source.unsplash.com/random"; 
 btn.addEventListener('click',()=>{
-   loadImage(url).then((data)=>container.appendChild(data)).catch((err)=>console.log(err));
+   addColor(1000,heading1,'red')
+   .then(()=>addColor(2000,heading2,'green'))
+   .then(()=>addColor(1000,heading3,'blue')).catch((err)=>console.log(err));
 });
 
 
-function loadImage(url){
-return new Promise((resolve,reject)=>{
-   let img = new Image();
-   img.addEventListener('load',()=>{
-resolve(img)
+function addColor (time,element,color){
+   return new Promise((resolve,reject)=>{
+      if(element){
+       setTimeout(()=>{
+      element.style.color = color;
+      // resolve();
+      },time)
+      }
+      else{
+         reject(new Error(`there is no such element ${element}`));
+      }
    })
-   img.addEventListener('error',()=>{
-reject(new Error (`failed to load the image from the source: ${url}`));
-   })
-   img.src = url;
-})
 }
