6b899614ff29be73bbbb9486a7d5ce2e7a338ae4
Milad64sh <m.shalikarian@gmail.com>
2022-05-16 09:34:36 -0700

    promise example

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
 const heading1 = document.querySelector('.one');
 const heading2 = document.querySelector('.two');
 const heading3 = document.querySelector('.three');
-
+const container = document.querySelector('.img-container');
 const btn = document.querySelector('.btn');
-
+const url = "https://source.unsplash.com/random"; 
 btn.addEventListener('click',()=>{
-
+   loadImage(url).then((data)=>container.appendChild(data)).catch((err)=>console.log(err));
 });
 
-const promise = new Promise((resolve,reject)=>{
-   let value = false;
-   if(value){
-      resolve('hey value is true');
-   }else{
-      reject(`there was an error, the value is false`)
-   }
-});
-promise.then((taco)=>{
-   console.log(taco);
-}).catch((err) =>{
-   console.log(err);
+
+function loadImage(url){
+return new Promise((resolve,reject)=>{
+   let img = new Image();
+   img.addEventListener('load',()=>{
+resolve(img)
+   })
+   img.addEventListener('error',()=>{
+reject(new Error (`failed to load the image from the source: ${url}`));
+   })
+   img.src = url;
 })
+}
