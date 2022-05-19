c2ab994b6e2a274f9a48fee461420a47a53160fb
Milad64sh <m.shalikarian@gmail.com>
2022-05-19 11:19:36 -0700

    promise

--- app.js
+++ app.js
@@ -4,25 +4,33 @@ const content = document.querySelector('.content');
 const URL = "https://api.chucknorris.io/jokes/random";
 
 btn.addEventListener('click', ()=>{
-getData(URL);
+getData(URL)
+.then(response => displayData(response))
+.catch((err) =>console.log(err));
 })
 
 
 function getData(url){
- const xhr = new XMLHttpRequest();
- xhr.open('GET',url);
- xhr.send();
- xhr.onreadystatechange = function(){
-  if(xhr.readyState !==4)return;
-  if(xhr.status === 200){
-   const {value:joke} = JSON.parse(xhr.responseText)
-   content.textContent = joke
-  }
-  else{
-   console.log({
-    status:xhr.status,
-    text:xhr.statusText,
-   });
-  }
- }
+ return new Promise((resolve,reject)=>{
+
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET',url);
+  xhr.send();
+  xhr.onreadystatechange = function(){
+   if(xhr.readyState !==4)return;
+   if(xhr.status === 200){
+   resolve(xhr.responseText);
+   }
+   else{
+    reject({
+     status:xhr.status,
+     text:xhr.statusText,
+    });
+   }
+  };
+ })
+}
+function displayData (data){
+     const {value:joke} = JSON.parse(data);
+    content.textContent = joke;
 }
