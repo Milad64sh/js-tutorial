7fa6d2724d6ac9a63a16cf0f7984186253de8476
Milad64sh <m.shalikarian@gmail.com>
2022-05-26 09:13:55 -0700

    async/await

--- DOMPomiseExample.js
+++ DOMPomiseExample.js
@@ -3,15 +3,11 @@ const heading2 = document.querySelector('.two');
 const heading3 = document.querySelector('.three');
 const btn = document.querySelector('.btn');
 
-btn.addEventListener('click', async ()=>{
-  try {
-    await addColor(1000,heading1,'red')
-    await addColor(2000,heading2,'blue')
-    await addColor(1000,heading3,'green')
-  }
-  catch (error) {
-    console.log(error);
-  }
+btn.addEventListener('click', ()=>{
+addColor(1000,heading1,'red')
+.then(()=> addColor(2000,heading2,'blue'))
+.then(()=> addColor(1000,heading3,'green'))
+.catch((err)=>console.log(err));
 });
 
 
