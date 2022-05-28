a19685c83ba3fd2f326c4e9d9bb1c1d0489291db
Milad64sh <m.shalikarian@gmail.com>
2022-05-28 15:12:34 -0700

    cocktails

--- app.js
+++ app.js
@@ -1,14 +1,1 @@
-import get from './utils/getElement.js';
-import getUser from './utils/fetch.js';
-import displayUser from './utils/displayUser.js';
-
-const btn = get('.btn');
-
-const showUser = async () => {
-  // get user from api
-  const person = await getUser();
-  displayUser(person);
-};
-
-window.addEventListener('DOMContentLoaded', showUser);
-btn.addEventListener('click', showUser);
+import getElement from './src/getElement.js';
