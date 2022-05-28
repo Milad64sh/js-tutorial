940f356d06a9466be48b721539ada2a38d33ecb2
Milad64sh <m.shalikarian@gmail.com>
2022-05-28 12:00:32 -0700

    display user module

--- app.js
+++ app.js
@@ -1,28 +1,9 @@
 import get from './utils/getElement.js';
 import getUser from './utils/fetch.js';
+import displayUser from './utils/displayUser.js';
 
-const img = get('.user-img');
-const title = get('.user-title');
-const value = get('.user-value');
 const btn = get('.btn');
-const btns = [...document.querySelectorAll('.icon')];
 
-const displayUser = (person) => {
-  img.src = person.image;
-  value.textContent = person.name;
-  title.textContent = `my name is`;
-  btns.forEach((btn) => btn.classList.remove('active'));
-  btns[0].classList.add('active');
-  btns.forEach((btn) => {
-    const label = btn.dataset.label;
-    btn.addEventListener('click', () => {
-      title.textContent = `my ${label} is`;
-      value.textContent = person[label];
-      btns.forEach((btn) => btn.classList.remove('active'));
-      btn.classList.add('active');
-    });
-  });
-};
 const showUser = async () => {
   // get user from api
   const person = await getUser();
