3d96b8733f42d5dfcb5923e6ef4fec5ac0b28278
Milad64sh <m.shalikarian@gmail.com>
2022-05-28 11:55:27 -0700

    display user

--- app.js
+++ app.js
@@ -7,10 +7,26 @@ const value = get('.user-value');
 const btn = get('.btn');
 const btns = [...document.querySelectorAll('.icon')];
 
+const displayUser = (person) => {
+  img.src = person.image;
+  value.textContent = person.name;
+  title.textContent = `my name is`;
+  btns.forEach((btn) => btn.classList.remove('active'));
+  btns[0].classList.add('active');
+  btns.forEach((btn) => {
+    const label = btn.dataset.label;
+    btn.addEventListener('click', () => {
+      title.textContent = `my ${label} is`;
+      value.textContent = person[label];
+      btns.forEach((btn) => btn.classList.remove('active'));
+      btn.classList.add('active');
+    });
+  });
+};
 const showUser = async () => {
   // get user from api
   const person = await getUser();
-  console.log(person);
+  displayUser(person);
 };
 
 window.addEventListener('DOMContentLoaded', showUser);
