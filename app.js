a15f95a10f220f0673598e11dfb35a34fba129f0
Milad64sh <m.shalikarian@gmail.com>
2022-05-29 10:58:14 -0700

    search form

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import getElement from './src/getElement.js';
 import presentsDrinks from './src/presentDrinks.js';
+import './src/searchForm.js';
 const URL = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=a';
 
 window.addEventListener('DOMContentLoaded', () => {
