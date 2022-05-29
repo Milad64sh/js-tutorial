b50f7c94de6cbd837e7832030600b5afc02ee467
Milad64sh <m.shalikarian@gmail.com>
2022-05-29 15:49:24 -0700

    loading

--- src/displayDrinks.js
+++ src/displayDrinks.js
@@ -1,9 +1,11 @@
 import get from './getElement.js';
+import { hideLoading } from './toggleLoading.js';
 
 const displayDrinks = ({ drinks }) => {
   const section = get('.section-center');
   const title = get('.title');
   if (!drinks) {
+    hideLoading();
     title.textContent = `sorry, no drinks matched your search`;
     section.innerHTML = null;
     return;
@@ -19,6 +21,7 @@ const displayDrinks = ({ drinks }) => {
       </a>`;
     })
     .join('');
+  hideLoading();
   title.textContent = '';
   section.innerHTML = newDrinks;
   return section;
