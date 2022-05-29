481be06708d17fa3fd75183804d302b1df8cdc74
Milad64sh <m.shalikarian@gmail.com>
2022-05-29 10:29:49 -0700

    display drinks

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
 import getElement from './src/getElement.js';
+import presentsDrinks from './src/presentDrinks.js';
+const URL = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=a';
+
+window.addEventListener('DOMContentLoaded', () => {
+  presentsDrinks(URL);
+});
