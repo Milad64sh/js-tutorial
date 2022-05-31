87b512c8e373fabf7cced4f973cb7ce0fec80694
Milad64sh <m.shalikarian@gmail.com>
2022-05-31 15:40:31 -0700

    submenu complete

--- app.js
+++ app.js
@@ -41,8 +41,38 @@ linkBtns.forEach((btn) => {
     const tempBtn = e.currentTarget.getBoundingClientRect();
     const center = (tempBtn.left + tempBtn.right) / 2;
     const bottom = tempBtn.bottom - 3;
-    submenu.classList.add('show');
-    submenu.style.left = `${center}px`;
-    submenu.style.top = `${bottom}px`;
+
+    const tempPage = sublinks.find(({ page }) => page === text);
+    if (tempPage) {
+      const { page, links } = tempPage;
+      submenu.classList.add('show');
+      submenu.style.left = `${center}px`;
+      submenu.style.right = `${bottom}px`;
+
+      // OPTIONAL
+      let columns = 'col-2';
+      if (links.length === 3) {
+        columns = 'col-3';
+      }
+      if (links.length > 3) {
+        columns = 'col-4';
+      }
+      submenu.innerHTML = `
+      <section>
+      <h4>${page}</h4>
+      <div class="submenu-center ${columns}">
+      ${links
+        .map((link) => {
+          return `<a href="${link.url}"><i class="${link.icon}"></i> ${link.label}</a>`;
+        })
+        .join('')}
+      </div>
+      </section>`;
+    }
   });
+});
+hero.addEventListener('mouseover', function (e) {
+  if (!e.target.classList.contains('link-btn')) {
+    submenu.classList.remove('show');
+  }
 });
