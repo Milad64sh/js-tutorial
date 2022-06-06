90fda12ed8f07565180139c5c223e70b9b1f9710
Milad64sh <m.shalikarian@gmail.com>
2022-06-06 15:50:49 -0700

    total price and amount

--- src/cart/setupCart.js
+++ src/cart/setupCart.js
@@ -27,11 +27,28 @@ export const addToCart = (id) => {
     addToCartDOM(product);
   } else {
   }
+  // add one to the item count
+  displayCartItemCount();
+  // display cart totals
+  displayCartTotal();
+  // set cart local storage
+  setStorageItem('cart', cart);
 
   // more stuff coming up
   openCart();
 };
-
+function displayCartItemCount() {
+  const amount = cart.reduce((total, cartItem) => {
+    return (total += cartItem.amount);
+  }, 0);
+  cartItemCountDOM.textContent = amount;
+}
+function displayCartTotal() {
+  let total = cart.reduce((total, cartItem) => {
+    return (total += cartItem.price * cartItem.amount);
+  }, 0);
+  cartTotalDOM.textContent = `Total :  ${formatPrice(total)}`;
+}
 const init = () => {
   console.log(cart);
 };
